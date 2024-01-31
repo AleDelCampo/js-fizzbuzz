d27855ba006061c7ece399c5754e27753eb08084
AleHashirama <alehashirama@gmail.com>
2024-01-31 17:14:23 +0100

    esercizio completo + bonus

--- script.js
+++ script.js
@@ -10,13 +10,29 @@ for(let i = 1; i <= 100 ; i++) {
     newElement.innerHTML = i;
     newElement.className = "square";
     newElement.style.color = "blue"; 
-    newElement.style.fontSize = "28px";
+    newElement.style.backgroundColor = "fuchsia";
+    newElement.style.fontSize = "24px";
 
     
     if(i % 3 == 0) {
         newElement.className += " three";
+        newElement.style.backgroundColor = "lightblue";
+        let three = newElement.textContent = "Fizz" 
     }
 
+    if(i % 5 == 0) {
+        newElement.className += " five";
+        let three = newElement.textContent = "Buzz"
+        newElement.style.color = "black";
+        newElement.style.backgroundColor = "aqua";
+    }
+
+    if(i % 15 == 0) {
+        newElement.className += " fifteen";
+        let three = newElement.textContent = "FizzBuzz"
+        newElement.style.color = "black";
+        newElement.style.backgroundColor = "blue";
+    }
     
     tableElement.append(newElement);
 
